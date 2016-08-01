9b5c14ccc765ef39221e3dcc68758c949eb0580f
Laurent Mallet <laurent.mallet@gmail.com>
2016-08-01 14:31:23 +0200

    a little script to remove .svn directories

---
+++ scripts/cleanSVN.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import shutil
+
+# Code by Laurent Mallet aka ellis2323 in 2014
+
+# Licence
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+def removeSVNDirs(path):
+    """Path """
+    res = []
+    for r, dirs, files in os.walk(path):
+        for d in dirs:
+            fullpath = os.path.join(r, d)
+            if d == ".svn":
+                shutil.rmtree(fullpath)
+
+
+if __name__=='__main__':
+    # get current dir
+    path = os.getcwd()
+    removeSVNDirs(path)
